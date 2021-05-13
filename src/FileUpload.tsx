aca8cca1dc00a0bbfd036b7fe9fee57445620e66
rudycrespo <rudy.crespo20@gmail.com>
2021-05-13 00:10:29 -0400

    styling file upload

--- src/FileUpload.tsx
+++ src/FileUpload.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "@emotion/styled";
 
 interface FileUploadProps {
   uploadText: string;
@@ -8,16 +9,25 @@ interface FileUploadProps {
   handleFileDrop: React.DragEventHandler<HTMLDivElement>
 }
 
+const DashedContainer = styled.div`
+  border: 3px dashed;
+  border-radius: .25rem;
+`
+
 const FileUpload = ({ uploadText, uploadAltText, fileTypeHelpText, handleFileUpload, handleFileDrop }: FileUploadProps) => {
   return (
     <div className="card">
-      <div className="card-content" onDrop={handleFileDrop}>
-        <label className="has-text-link is-clickable" htmlFor="file-input">
-          {uploadText}
-          <input className="is-sr-only" type="file" name="file-input" id="file-input" onChange={handleFileUpload}/>
-        </label>
-        <p>or { uploadAltText }</p>
-        <p>{ fileTypeHelpText }</p>
+      <div className="card-content">
+        <DashedContainer className="media" onDrop={handleFileDrop}>
+          <div className="media-content has-text-centered p-5">
+            <label className="has-text-link is-clickable" htmlFor="file-input">
+              {uploadText}
+              <input className="is-sr-only" type="file" name="file-input" id="file-input" onChange={handleFileUpload}/>
+            </label>
+            <p>or { uploadAltText }</p>
+            <p className="has-text-grey">{ fileTypeHelpText }</p>
+          </div>
+        </DashedContainer>
       </div>
     </div>
   )
