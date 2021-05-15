67a7c4bfd0b525e2d577ccca7a73aea0108c5365
rudycrespo <rudy.crespo20@gmail.com>
2021-05-15 10:40:38 -0400

    setting up hook for postdata

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import FileUpload from "./FileUpload";
 import BaseCard from "./BaseCard";
+import IPost from "./data/interfaces/IPost";
 
 function App() {
-  const getFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.files)
+  const [postsData, setPostsData] = React.useState<IPost[]>([])
+
+  const getFileUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    try {
+      const { files } = e.target
+
+      if (files) {
+        const uploadedFileText = await files[0].text()
+
+        console.log(uploadedFileText)
+      }
+    } catch (e) {
+      throw new Error(e.message)
+    }
   }
 
   const getFileDrop = async (e: React.DragEvent) => {
     e.preventDefault()
     e.stopPropagation();
 
-    console.log(await e.dataTransfer.files[0].text())
+    try {
+      const { files } = e.dataTransfer
+
+      if (files) {
+        const droppedFile = await files[0].text()
+
+        console.log(droppedFile)
+      }
+    } catch (e) {
+      throw new Error(e.message)
+    }
   }
 
   return (
